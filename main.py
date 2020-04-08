122c00ae3b892d64c792558c0a057543fb06a979
Rajat Nagpure <rajatnagpureiiest@gmail.com>
2020-04-08 20:06:06 +0530

    code optimisation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from browser import website
 from constants import *
-import os
 
 if __name__ == '__main__':
     procap = website()
     procap.login()
     procap.get_call()
+    procap.stop_browser()
